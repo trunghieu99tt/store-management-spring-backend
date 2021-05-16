28d5258edabecced7b03d79358c16f725c5d3c1d
haianh193 <haianhnguyen1903@gmail.com>
2021-05-16 19:13:55 +0700

    create financialPlan-service

--- src/main/java/com/projects/app/models/request/FinancialPlanDTO.java
+++ src/main/java/com/projects/app/models/request/FinancialPlanDTO.java
@@ -1,6 +1,7 @@
-package com.projects.app.models;
+package com.projects.app.models.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.projects.app.models.Budget;
 import com.projects.app.models.user.Manager;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
@@ -10,21 +11,11 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-import java.util.Date;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class FinancialPlan {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(description = "financialPlanID", hidden = true)
-    @JsonProperty(value = "id", access = JsonProperty.Access.READ_ONLY)
-    private Long id;
-
-
+public class FinancialPlanDTO {
     @NotNull
     @Positive(message = "Total must be positive")
     private float total;
@@ -33,21 +24,13 @@ public class FinancialPlan {
     @Size(min = 1, max = 500, message = "Description must not be longer than 500 characters")
     private String description;
 
-    @ManyToOne
-    @JoinColumn(name = "budgetID")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Budget budget;
+    @NotNull
+    private Long budgetID;
 
-    @ManyToOne
-    @JoinColumn(name = "managerID")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Manager manager;
+    @NotNull
+    private Long managerID;
 
     @NotBlank(message = "Please provide name for FinancialPlan")
     @Size(min = 1, max = 50, message = "name must not be longer than 50 characters")
     private String name;
-
-    private Date createDate;
 }
