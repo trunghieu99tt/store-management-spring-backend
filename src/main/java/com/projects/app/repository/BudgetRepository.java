d607a6b1819c424f8233612373179346500942c1
ntheanh201 <ntheanh201@gmail.com>
2021-05-25 11:00:28 +0700

    fix generate report

--- src/main/java/com/projects/app/repository/BudgetRepository.java
+++ src/main/java/com/projects/app/repository/BudgetRepository.java
@@ -3,6 +3,8 @@ package com.projects.app.repository;
 import com.projects.app.models.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
     Budget findBudgetByMonthAndYear(int month, int year);
 }
